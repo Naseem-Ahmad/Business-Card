31a377a713c7349253c498c3d7c0b8237ed8102a
Naseem Ahmad <naseemahmad1998@gmail.com>
2025-10-18 11:57:39 +0530

    Modified  the skill & Added the product section

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,28 @@ export default function App() {
     <img src={NA} className="profile" alt="#" />
     <h2><strong>Naseem Ahmad</strong></h2>
     <p className="desgination">Full Stack Developer</p>
-    <a href="https://www.creativetract.com/">CreativeTract.com</a><br/><br/>
+    <a className="website" href="https://www.creativetract.com/">CreativeTract.com</a><br/><br/>
+
     <a href="mailto:naseemahmad6504@gmail.com"><img src={NewMail} className="mail" width="140px" alt="#" /></a>
     <a href="https://www.linkedin.com/in/naseem-ahmad-978a21190"><img src={newlink} className="linkedin" width="140px" alt="#" />
 </a>
     
-    <h1 align="Left">About:</h1>
-    <p align="justify">Software craftsman 🖥️ | C#, ASP.NET & ReactJS are my tools, coffee is my fuel ☕ | Turning complex problems into simple solutions, automating the boring stuff, and keeping bugs on a short leash 🐍</p>
-  <h1 align="Left">Skill:</h1>
-  <p align="justify">
-   Code whisperer in C#, Java & WebApi ⚡ | ASP.NET & ReactJS fan 🤓 | SQL tamer 🐍 | APIs & automation wizard ✨ | Loves clean code, coffee, and making bugs cry 🐞💥 
-  </p>
+  <h2 className= "about">About:</h2>
+    <p align="justify">Software craftsman 🖥️ | Turning complex problems into simple solutions, automating the boring stuff, and keeping bugs on a short leash 🐍 Loves clean code, coffee, and making bugs cry 🐞💥 
+</p>
+  <h2 className= "skill">Skill:</h2>
+  <ul>
+   <li className="hide-me">🧑‍💻 Mastering scalable solutions with C#, ASP.NET & ReactJS</li>
+   <li className="hide-me">🧠 Turning complex challenges into simple/maintainable & scalable code</li>
+   <li className="hide-me">🤖 Automating the boring, exploring smart systems & autonomous tech</li>
+   <li className="hide-me">⚙️ Blending software engineering with emerging tech like AI & robotics</li>
+   <li className="hide-me">🚀 Not just building apps - building smarter, future-ready systems</li>
+   </ul>
+
+  <p className="product">Products: <a href="https://ingreedyent.creativetract.com/">Ingreedyent Chef </a>  👈 Click Here</p>
+     
   <br />
-   <br />
+
   <footer>
   © 2025 All Rights Reserved
   </footer>
