cb9e3450649eefc986104e883322182bc8dedb26
Naseem Ahmad <naseemahmad1998@gmail.com>
2025-10-22 21:28:23 +0530

    basic structure2

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ export default function App() {
     <a className="website" href="https://www.creativetract.com/">CreativeTract.com</a><br/><br/>
 
     <a href="mailto:naseemahmad6504@gmail.com"><img src={NewMail} className="mail" width="140px" alt="#" /></a>
-    <a href="https://www.linkedin.com/in/naseem-ahmad-978a21190"><img src={newlink} className="mail" width="140px" alt="#" />
-</a>
+    <a href="https://www.linkedin.com/in/naseem-ahmad-978a21190"><img src={newlink} className="mail" width="140px" alt="#" /></a>
+       <a href="#"><img src={travel} className="myTravelJournal" width="140px" alt="My travel logo" /></a>
     
   <h2 className= "about">About:</h2>
     <p align="justify">Software craftsman 🖥️ | Turning complex problems into simple solutions, automating the boring stuff, and keeping bugs on a short leash 🐍 Loves clean code, coffee, and making bugs cry 🐞💥 
